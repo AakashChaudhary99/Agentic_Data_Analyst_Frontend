13c028c108c8737e47b71615f89590f0829ba593
Aakash Kumar Chaudhary <aakaas.c.4508@gmail.com>
2026-02-02 18:22:59 +0530

    frontend code updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,56 @@ import './App.css'
 function App() {
   const [file, setFile] = useState(null)
   const [output, setOutput] = useState(null)
+  const [outputText, setOutputText] = useState(null)
   const [instruction, setInstruction] = useState(null)
+  const [error, setError] = useState("")
 
   function changeFile(e) {
+    setError("")
     let f = e.target.files[0]
+    if(!(f.name.endsWith('.csv') || f.name.endsWith('.xlsx'))){
+      setError("Only CSV and Excel are supported")
+      return
+    }
     setFile(f)
   }
 
   async function uploadFile() {
     try {
-      console.log("BTn clicked")
       let form = new FormData()
       form.append('file', file)
       form.append('instruction', instruction)
 
-      let resp = await fetch("api/processing",
+      let resp = await fetch("http://127.0.0.1:8000/processing",
         {
           method: "POST",
           body: form
         }
       );
-      const blob = await resp.blob();
-      const url = window.URL.createObjectURL(blob);
-      setOutput(url)
+      const contentType = resp.headers.get("content-type");
+
+      if (contentType?.includes("text/csv")){
+        const blob = await resp.blob();
+        const url = window.URL.createObjectURL(blob);
+        setOutput(url)
+
+      }
+      else{
+        const text = await resp.text();
+        setOutputText(text)
+      }
     } catch (error) {
       console.log("Error in upload", error)
     }
   }
 
   return (
-    <div style={{display:'flex'}}>
+    <div style={{
+      width:'100%',
+      display:'flex',
+      height:'100%',
+      justifyContent:"center"
+      }}>
       <div style={{
         width:'30%',
         display:'flex',
@@ -43,12 +63,22 @@ function App() {
       }}>
 
         <input type='file' onChange={changeFile} />
-        <button style={{width:'100px'}} onClick={uploadFile}>Upload</button>
         {
-          file && (
-            <input onChange={(e) => setInstruction(e.target.value)} placeholder='Please enter your instruction' />
+          error && (
+            <span style={{color:'red'}}>{error}</span>
           )
         }
+        
+        {
+          file && (
+            <textarea rows="4" onChange={(e) => setInstruction(e.target.value)} placeholder='Please enter your question or instruction' />
+          )
+        }
+        <button style={{width:'100px'}} onClick={uploadFile}
+        disabled={!file || error || !instruction}
+        >
+          Upload
+        </button>
       </div>
 
       {output && (
@@ -56,6 +86,11 @@ function App() {
           Download CSV
         </a>
       )}
+      {
+        outputText && (
+          <div>{outputText}</div>
+        )
+      }
     </div>
   )
 }
